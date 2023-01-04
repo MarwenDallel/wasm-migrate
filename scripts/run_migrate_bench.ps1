c90d10d1ecb5ca2b9c76517a9e36de33d32d17c8
Marwen Dallel <71770363+MarwenDallel@users.noreply.github.com>
2023-01-04 20:40:16 +0100

    Fix cross-migration bench

--- scripts/run_migrate_bench.ps1
+++ scripts/run_migrate_bench.ps1
@@ -2,8 +2,13 @@ $migrationSequence = @("wasmMigrateClientB", "wasmMigrateClientC", "wasmMigrateC
 
 $currentClient = "wasmMigrateClientA"
 
+& "wasm-migrate" start wasmMigrateClientA ./wasm/fibonacci.async.wasm fibonacci 46
+Start-Sleep -Seconds 5
+
 foreach ($nextClient in $migrationSequence) {
+  # Print the command to be executed
+  Write-Host "wasm-migrate migrate $currentClient $nextClient"
   & "wasm-migrate" migrate $currentClient $nextClient
   $currentClient = $nextClient
-  Start-Sleep -Seconds 1
+  Start-Sleep -Seconds 5
 }
--- scripts/run_migrate_bench.sh
+++ scripts/run_migrate_bench.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Translate this JS to sh
 # const MIGRATION_SEQUENCE = ["wasmMigrateClientB", "wasmMigrateClientC", "wasmMigrateClientB", "wasmMigrateClientA"];
 
@@ -6,11 +7,15 @@ migrationSequence=("wasmMigrateClientB" "wasmMigrateClientC" "wasmMigrateClientB
 # Use a variable to store the current client
 currentClient="wasmMigrateClientA"
 
+echo "Start command issued to client WasmMigrateClientA"
+wasm-migrate start wasmMigrateClientA ./wasm/fibonacci.async.wasm fibonacci 46
+sleep 5
+
 # For each client in the sequence, run the migration command "wasm-migrate migrate currentClient nextClient"
 # Set currentclient to the next client in the sequence after the migration
 # wait 1 second between migrations
 for nextClient in "${migrationSequence[@]}"; do
   wasm-migrate migrate $currentClient $nextClient
   currentClient=$nextClient
-  sleep 1
+  sleep 5
 done
